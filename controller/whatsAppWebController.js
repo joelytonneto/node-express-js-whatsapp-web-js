081909d5a7ad11cbf089cdf217e51b5f3cdbd41d
Joelyton <joelytonneto@gmail.com>
2023-08-20 15:14:36 -0300

    Foi inserido mais algumas opções de envio de mensagem, bem como interação para chatbot.

--- controller/whatsAppWebController.js
+++ controller/whatsAppWebController.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const client = require('../utils/whatsApp/whatsappManager');
+const { Location, List, Buttons, MessageMedia } = require('whatsapp-web.js');
 
 exports.enviarMensagemWhatsAppWeb = async (req, res, next) => {
     const { numero, mensagem, token } = req.body;
@@ -17,13 +18,83 @@ exports.enviarMensagemWhatsAppWeb = async (req, res, next) => {
         numeroFormatado = numero.slice(0, 4) + numero.slice(5) + '@c.us';
     } else {
         numeroFormatado = numero + '@c.us';
-    }    
-    
+    }
+
+    // Enviar mensagem comum personalisável
     client.sendMessage(numeroFormatado, mensagem)
         .then(response => {            
             res.status(200).json({ message: 'Mensagem enviada com sucesso!' });
         })
         .catch(error => {
             res.status(500).json({ error: 'Erro ao enviar mensagem.' });
-        });
+        });    
+    
+    // Envio de imagem 
+    // const media = MessageMedia.fromFilePath(`assets/images/pin.png`);
+    // client.sendMessage(numeroFormatado, media, { caption: 'Pinguim' })
+    //     .then(response => {            
+    //         res.status(200).json({ message: 'Mensagem enviada com sucesso!' });
+    //     })
+    //     .catch(error => {
+    //         res.status(500).json({ error: 'Erro ao enviar mensagem.' });
+    //     });
+
+    // Envio de imagem Visualização Única
+    // const media = MessageMedia.fromFilePath(`assets/images/pin.png`);
+    // client.sendMessage(numeroFormatado, media, { isViewOnce: true })
+    //     .then(response => {            
+    //         res.status(200).json({ message: 'Mensagem enviada com sucesso!' });
+    //     })
+    //     .catch(error => {
+    //         res.status(500).json({ error: 'Erro ao enviar mensagem.' });
+    //     });
+
+    // Envio de áudio normal apenas como arquivo
+    // const media = MessageMedia.fromFilePath(`assets/sounds/alerta1.mp3`);
+    // client.sendMessage(numeroFormatado, media)
+    //     .then(response => {            
+    //         res.status(200).json({ message: 'Mensagem enviada com sucesso!' });
+    //     })
+    //     .catch(error => {
+    //         res.status(500).json({ error: 'Erro ao enviar mensagem.' });
+    //     });
+
+    // Envio de áudio como mensagem de Voz
+    // const media = MessageMedia.fromFilePath(`assets/sounds/alerta1.mp3`);
+    // client.sendMessage(numeroFormatado, media, { sendAudioAsVoice: true })
+    //     .then(response => {            
+    //         res.status(200).json({ message: 'Mensagem enviada com sucesso!' });
+    //     })
+    //     .catch(error => {
+    //         res.status(500).json({ error: 'Erro ao enviar mensagem.' });
+    //     });
+
+    // Mandar Localização
+    // client.sendMessage(numeroFormatado, new Location(-3.791789297734254, -38.47091540021312, 'AG Autos Service\nOficina mecânica'))
+    //     .then(response => {            
+    //         res.status(200).json({ message: 'Mensagem enviada com sucesso!' });
+    //     })
+    //     .catch(error => {
+    //         res.status(500).json({ error: 'Erro ao enviar mensagem.' });
+    //     });
+
+    // Envio de Lista, Não é tão simples, precisa entrar em contato com WhatsApp Bussiness para aprovação
+    // let sections = [{ title: 'sectionTitle', rows: [{ title: 'ListItem1', description: 'desc' }, { title: 'ListItem2' }] }];
+    // let list = new List('List body', 'btnText', sections, 'Title', 'footer');
+    // client.sendMessage(numeroFormatado, list)
+    //     .then(response => {            
+    //         res.status(200).json({ message: 'Mensagem enviada com sucesso!' });
+    //     })
+    //     .catch(error => {
+    //         res.status(500).json({ error: 'Erro ao enviar mensagem.'+ error });
+    //     });
+    
+    // Envio de Botões, Não é tão simples, precisa entrar em contato com WhatsApp Bussiness para aprovação
+    // client.sendMessage(numeroFormatado, new Buttons('Button body', [{ body: 'bt1' }, { body: 'bt2' }, { body: 'bt3' }], 'title', 'footer'))
+    //     .then(response => {            
+    //         res.status(200).json({ message: 'Mensagem enviada com sucesso!' });
+    //     })
+    //     .catch(error => {
+    //         res.status(500).json({ error: 'Erro ao enviar mensagem.' });
+    //     });    
 };
--- utils/whatsApp/whatsappManager.js
+++ utils/whatsApp/whatsappManager.js
@@ -1,5 +1,5 @@
 const qrcode = require('qrcode-terminal');
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Client, LocalAuth, Location, List, Buttons, MessageMedia } = require('whatsapp-web.js');
 
 const client = new Client({
     authStrategy: new LocalAuth({
@@ -18,6 +18,208 @@ client.on('qr', qr => {
 
 client.on('ready', () => {
     console.log('Cliente está pronto!');
+});
+
+client.on('message', async msg => {
+    console.log('MESSAGE RECEIVED', msg);
+
+    if (msg.body === '!ping reply') {
+        // Envia uma nova mensagem como resposta à atual
+        msg.reply('pong');
+
+    } else if (msg.body === '!ping') {        
+        // Envia uma nova mensagem para o mesmo chat
+        client.sendMessage(msg.from, 'pong');
+
+    } else if (msg.body.startsWith('!sendto ')) {
+        // Envio direto de uma nova mensagem para telefone específico
+        let number = msg.body.split(' ')[1];
+        let messageIndex = msg.body.indexOf(number) + number.length;
+        let message = msg.body.slice(messageIndex, msg.body.length);
+        number = number.includes('@c.us') ? number : `${number}@c.us`;
+        let chat = await msg.getChat();
+        chat.sendSeen();
+        client.sendMessage(number, message);
+
+    } else if (msg.body.startsWith('!subject ')) {
+        // Muda o nome do grupo
+        let chat = await msg.getChat();
+        if (chat.isGroup) {
+            let newSubject = msg.body.slice(9);
+            chat.setSubject(newSubject);
+        } else {
+            msg.reply('Este comando só pode ser usado em grupo!');
+        }
+    } else if (msg.body.startsWith('!echo ')) {
+        // Responde com a mesma mensagem
+        msg.reply(msg.body.slice(6));
+    } else if (msg.body.startsWith('!desc ')) {
+        // Muda a descrição do grupo
+        let chat = await msg.getChat();
+        if (chat.isGroup) {
+            let newDescription = msg.body.slice(6);
+            chat.setDescription(newDescription);
+        } else {
+            msg.reply('Este comando só pode ser usado em grupo!');
+        }
+    } else if (msg.body === '!leave') {
+        // Sai do grupo
+        let chat = await msg.getChat();
+        if (chat.isGroup) {
+            chat.leave();
+        } else {
+            msg.reply('Este comando só pode ser usado em grupo!');
+        }
+    } else if (msg.body.startsWith('!join ')) {
+        const inviteCode = msg.body.split(' ')[1];
+        try {
+            await client.acceptInvite(inviteCode);
+            msg.reply('Entrou no grupo!');
+        } catch (e) {
+            msg.reply('Esse código de convite parece ser inválido.');
+        }
+    } else if (msg.body === '!groupinfo') {
+        let chat = await msg.getChat();
+        if (chat.isGroup) {
+            msg.reply(`
+                *Detalhes do Grupo*
+                Nome: ${chat.name}
+                Descrição: ${chat.description}
+                Criado em: ${chat.createdAt.toString()}
+                Criado por: ${chat.owner.user}
+                Qtd. de Participantes ${chat.participants.length}
+            `);
+        } else {
+            msg.reply('Este comando só pode ser usado em grupo!');
+        }
+    } else if (msg.body === '!chats') {
+        const chats = await client.getChats();
+        client.sendMessage(msg.from, `O bot tem ${chats.length} chats abertos.`);
+    } else if (msg.body === '!info') {
+        let info = client.info;
+        client.sendMessage(msg.from, `
+            *Informações da Conexão*
+            Nome de Usuário: ${info.pushname}
+            Meu número: ${info.wid.user}
+            Plataforma: ${info.platform}
+        `);
+    } else if (msg.body === '!mediainfo' && msg.hasMedia) {
+        const attachmentData = await msg.downloadMedia();
+        msg.reply(`
+            *Informações da Mídia*
+            Tipo da Imagem: ${attachmentData.mimetype}
+            Nome do Arquivo: ${attachmentData.filename}
+            Tamanho do arquivo: ${attachmentData.data.length}
+        `);
+    } else if (msg.body === '!quoteinfo' && msg.hasQuotedMsg) {
+        const quotedMsg = await msg.getQuotedMessage();
+
+        quotedMsg.reply(`
+            ID: ${quotedMsg.id._serialized}
+            Tipo: ${quotedMsg.type}
+            Autor: ${quotedMsg.author || quotedMsg.from}
+            Timestamp: ${quotedMsg.timestamp}
+            É uma mídia? ${quotedMsg.hasMedia}
+        `);
+    } else if (msg.body === '!resendmedia' && msg.hasQuotedMsg) {
+        const quotedMsg = await msg.getQuotedMessage();
+        if (quotedMsg.hasMedia) {
+            const attachmentData = await quotedMsg.downloadMedia();
+            client.sendMessage(msg.from, attachmentData, { caption: 'Aqui está a mídia solicitada.' });
+        }
+        if (quotedMsg.hasMedia && quotedMsg.type === 'audio') {
+            const audio = await quotedMsg.downloadMedia();
+            await client.sendMessage(msg.from, audio, { sendAudioAsVoice: true });
+        }
+    } else if (msg.body === '!isviewonce' && msg.hasQuotedMsg) {
+        const quotedMsg = await msg.getQuotedMessage();
+        if (quotedMsg.hasMedia) {
+            const media = await quotedMsg.downloadMedia();
+            await client.sendMessage(msg.from, media, { isViewOnce: true });
+        }
+    } else if (msg.body === '!location') {
+        msg.reply(new Location(37.422, -122.084, 'Googleplex\nGoogle Headquarters'));
+    } else if (msg.location) {
+        msg.reply(msg.location);
+    } else if (msg.body.startsWith('!status ')) {
+        const newStatus = msg.body.split(' ')[1];
+        await client.setStatus(newStatus);
+        msg.reply(`O status foi atualizado para *${newStatus}*`);
+    } else if (msg.body === '!mention') {
+        const contact = await msg.getContact();
+        const chat = await msg.getChat();
+        chat.sendMessage(`Olá @${contact.number}!`, {
+            mentions: [contact]
+        });
+    } else if (msg.body === '!delete') {
+        if (msg.hasQuotedMsg) {
+            const quotedMsg = await msg.getQuotedMessage();
+            if (quotedMsg.fromMe) {
+                quotedMsg.delete(true);
+            } else {
+                msg.reply('Só posso deletar minhas próprias mensagens');
+            }
+        }
+    } else if (msg.body === '!pin') {
+        const chat = await msg.getChat();
+        await chat.pin();
+    } else if (msg.body === '!archive') {
+        const chat = await msg.getChat();
+        await chat.archive();
+    } else if (msg.body === '!mute') {
+        const chat = await msg.getChat();
+        // silencia o chat por 20 segundos
+        const unmuteDate = new Date();
+        unmuteDate.setSeconds(unmuteDate.getSeconds() + 20);
+        await chat.mute(unmuteDate);
+    } else if (msg.body === '!typing') {
+        const chat = await msg.getChat();
+        // simula a digitação no chat
+        chat.sendStateTyping();
+    } else if (msg.body === '!recording') {
+        const chat = await msg.getChat();
+        // simula gravação de áudio no chat
+        chat.sendStateRecording();
+    } else if (msg.body === '!clearstate') {
+        const chat = await msg.getChat();
+        // para de digitar ou gravar no chat
+        chat.clearState();
+    } else if (msg.body === '!jumpto') {
+        if (msg.hasQuotedMsg) {
+            const quotedMsg = await msg.getQuotedMessage();
+            client.interface.openChatWindowAt(quotedMsg.id._serialized);
+        }
+    } else if (msg.body === '!buttons') {
+        let button = new Buttons('Button body', [{ body: 'bt1' }, { body: 'bt2' }, { body: 'bt3' }], 'title', 'footer');
+        client.sendMessage(msg.from, button);
+    } else if (msg.body === '!list') {
+        let sections = [{ title: 'sectionTitle', rows: [{ title: 'ListItem1', description: 'desc' }, { title: 'ListItem2' }] }];
+        let list = new List('List body', 'btnText', sections, 'Title', 'footer');
+        client.sendMessage(msg.from, list);
+    } else if (msg.body === '!reaction') {
+        msg.react('👍');
+    } else if (msg.body === '!edit') {
+        if (msg.hasQuotedMsg) {
+            const quotedMsg = await msg.getQuotedMessage();
+            if (quotedMsg.fromMe) {
+                quotedMsg.edit(msg.body.replace('!edit', ''));
+            } else {
+                msg.reply('Só posso editar minhas próprias mensagens');
+            }
+        }
+    } else if (msg.body === '!updatelabels') {
+        const chat = await msg.getChat();
+        await chat.changeLabels([0, 1]);
+    } else if (msg.body === '!addlabels') {
+        const chat = await msg.getChat();
+        let labels = (await chat.getLabels()).map(l => l.id);
+        labels.push('0');
+        labels.push('1');
+        await chat.changeLabels(labels);
+    } else if (msg.body === '!removelabels') {
+        const chat = await msg.getChat();
+        await chat.changeLabels([]);
+    }
 });
 
 client.initialize();
