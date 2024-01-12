65928b21271988aea715044e6792e7eaca221527
Joelyton <joelytonneto@gmail.com>
2024-01-12 16:27:05 -0300

    feat(whatsAppWebController.js)   - Adicionado no controller, novo método para envio de PDF (base 64).
    feat(api)
      - Criado nova requisição /enviar-pdf

--- controller/whatsAppWebController.js
+++ controller/whatsAppWebController.js
@@ -47,6 +47,7 @@ exports.enviarMensagemWhatsAppWeb = async (req, res, next) => {
         });
 
     //Envio de PDF
+    //|ENVIAR PDF COMO BASE 64|// const media = new MessageMedia('application/pdf', string_base64, caption_text);
     // const media = await MessageMedia.fromUrl('https://logos-cliente.s3.us-east-2.amazonaws.com/propostaSimulador.pdf');    
     // client.sendMessage(numeroFormatado, media, { caption: 'PDF' })
     //     .then(response => {            
@@ -124,4 +125,44 @@ exports.enviarMensagemWhatsAppWeb = async (req, res, next) => {
     //     .catch(error => {
     //         res.status(500).json({ error: 'Erro ao enviar mensagem.' });
     //     });    
+};
+
+exports.enviarPDFWhatsAppWeb = async (req, res, next) => {
+    const { numero, nomeArquivo, mensagem, token, base64 } = req.body;
+
+    if (!numero || !nomeArquivo || !mensagem || !token || !base64) {
+        return res.status(400).json({ error: 'Número, nome do arquivo, mensagem, base64 e token são obrigatórios.' });
+    }
+
+    if (token != process.env.TOKEN_API_WHATSAPP) {
+        return res.status(400).json({ error: 'Token está inválido' });
+    }    
+
+    let numeroFormatado = numero;
+
+    let to = numeroFormatado;
+    let contactId;
+
+    if (to.startsWith('55') && to.length == 13 && to[4] == 9) {
+        contactId = await client.getNumberId(to.slice(0, 4) + to.slice(5));
+    }
+
+    if (!contactId) {
+        contactId = await client.getNumberId(to);
+    }
+      
+    if (contactId) {
+        to = contactId.user;
+    }
+
+    //Envio de PDF    
+    const media = new MessageMedia('application/pdf', base64, nomeArquivo);    
+    client.sendMessage(to + '@c.us', media, { caption: mensagem })
+        .then(response => {            
+            res.status(200).json({ message: 'Mensagem enviada com sucesso!' });
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ error: 'Erro ao enviar mensagem.' });
+        });
 };
--- routes/api/whatsAppWeb/api.js
+++ routes/api/whatsAppWeb/api.js
@@ -4,5 +4,6 @@ const routerWhatsAppWeb = express();
 const whatsAppWebController = require('../../../controller/whatsAppWebController');
 
 routerWhatsAppWeb.post('/enviar-mensagem', whatsAppWebController.enviarMensagemWhatsAppWeb);
+routerWhatsAppWeb.post('/enviar-pdf', whatsAppWebController.enviarPDFWhatsAppWeb);
 
 module.exports = routerWhatsAppWeb;
